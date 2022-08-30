171eb1eb3a330e689426297eb887f7e4c82ea2ed
VasckMe <kasarin.anton@gmail.com>
2022-08-30 17:41:13 +0300

    Added Actions Collection VC and Action Collection Cell

---
+++ REST API App/REST API App/Controllers/ActionsCollectionViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ActionsCollectionViewController.swift
+//  REST API App
+//
+//  Created by Apple Macbook Pro 13 on 30.08.22.
+//
+
+import UIKit
+
+enum Actions: String, CaseIterable {
+    case downloadImage = "Download Image"
+    case users = "Users"
+}
+
+class ActionsCollectionViewController: UICollectionViewController {
+
+    private let actions = Actions.allCases
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+//        self.collectionView!.register(ActionCollectionViewCell.self,
+//                                      forCellWithReuseIdentifier: ActionCollectionViewCell.identifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return actions.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard
+            let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: ActionCollectionViewCell.identifier,
+            for: indexPath) as? ActionCollectionViewCell
+        else {
+            return UICollectionViewCell()
+        }
+    
+        let action = actions[indexPath.row]
+        cell.ActionLabel.text = action.rawValue
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let action = actions[indexPath.row]
+//        switch action {
+//        case .downloadImage:
+//        case .users:
+//            
+//        }
+    }
+    
+    
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+}
+
+extension ActionsCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width - 20
+        return CGSize(width: (width), height: width / 2)
+    }
+}
