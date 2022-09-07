9a8575203c976c040b6e5bdd58c5e876a1160a02
VasckMe <kasarin.anton@gmail.com>
2022-09-07 13:05:16 +0300

    Added errors on NewPostVC

--- REST API App/REST API App/Controllers/Posts Controllers/NewPostViewController.swift
+++ REST API App/REST API App/Controllers/Posts Controllers/NewPostViewController.swift
@@ -10,14 +10,37 @@ import Alamofire
 import SwiftyJSON
 
 class NewPostViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet private weak var postTitleLabel: UITextField!
+    @IBOutlet private weak var postBodyTextView: UITextView!
+    @IBOutlet private weak var titleErrorLabel: UILabel!
+    @IBOutlet private weak var bodyErrorLabel: UILabel!
+    @IBOutlet private weak var savePostButton: UIButton!
+    
+    var isTitleRight = false {
+        didSet {
+            checkPostButton()
+        }
+    }
+    var isBodyRight = false {
+        didSet {
+            checkPostButton()
+        }
+    }
+    // MARK: - viewDidLoad
+    
+    override func viewDidLoad() {
+        super .viewDidLoad()
+        postBodyTextView.delegate = self
+    }
+    
+    // MARK: - Properties
     var delegate: UpdatePostsData?
     var userId: Int?
     
-    @IBOutlet weak var postTitleLabel: UITextField!
-    @IBOutlet weak var postBodyTextView: UITextView!
-    
-    @IBAction func savePostAction() {
+    // MARK: - IBActions
+    @IBAction private func savePostAction() {
         guard
             let url = ApiConstants.postsURL,
             let userId = userId,
@@ -39,5 +62,32 @@ class NewPostViewController: UIViewController {
                 print(error!)
             }
         }
+    }
+    
+    @IBAction private func titleAction() {
+        checkTitleForError()
+    }
+    
+    // MARK: - Private functions
+    private func checkPostButton() {
+        savePostButton.isEnabled = isTitleRight && isBodyRight
+    }
+    private func checkTitleForError() {
+        if let title = postTitleLabel.text {
+            isTitleRight = !title.isEmpty
+        }
+        titleErrorLabel.isHidden = isTitleRight
+    }
+    private func checkBodyForError() {
+        if let body = postBodyTextView.text {
+            isBodyRight = !body.isEmpty && body.count >= 10
+        }
+        bodyErrorLabel.isHidden = isBodyRight
+    }
+}
+
+extension NewPostViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        checkBodyForError()
     }
 }
